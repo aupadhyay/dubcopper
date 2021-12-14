1f05c809f27b5a12d18122dc68a67282d837ce1e
Pranav Pomalapally <prpranav@berkeley.edu>
2021-12-14 15:18:16 -0800

    mapping working correctly (robustness)

--- controller_state_test/controller_state_test.ino
+++ controller_state_test/controller_state_test.ino
@@ -1,66 +1,31 @@
 #include <NewPing.h>
-
-#define MAX_DISTANCE 300
-
-//* change for drone
+#include <RunningMedian.h>
 
 /*
- * ULTRASONIC SENSORS (Trig, Echo Pin)
- * BACK - (0, 1)
- * FRONT - (2, 3)
+ * CONTROLLER_STATE
+ * 
+ * ULTRASONIC SENSORS (Index, Trig, Echo Pin)
+ * FRONT - (0, 2, 3)
+ * RIGHT - (1, 4, 5)
+ * BACK -  (2, 0, 1)
+ * LEFT -  (3, 6, 7)
  * 
  */
 
-// configs
-bool point_delay = false;
+#define MAX_DISTANCE 300
+#define NUM_SENSORS 4
+#define NUM_READS 3
+#define BLOCK_THRESHOLD 50
+#define OFF_SPEED 0         // START
+#define LIFTOFF_TIME 0      // LIFTOFF 3000
+#define FORWARD_SPEED 75    // MOVE_FORWARD
+#define LOWER_SPEED 50      // LOWER
+#define LIFT_SPEED 90       // LIFTOFF (0 for kabuki)
 
-// start
-bool armed = false;
-bool lblock = false;
-bool fblock = false;
-const int off_speed = 0;
+#define WALL_LEFT_THRESH 3
+#define OPEN_THRESH 3
 
-// liftoff
-int liftoff_counter = 0;
-const int lift_speed = 90; // technically should be zero for 2D kabuku
-const int LIFTOFF_TIME = 0; // 3000 ms
-
-// move_forward
-int forward_counter = 0;
-const int forward_speed = 75;
-
-// wall_left
-int orientation = 0;
-int num_turns = 0;
-int curr_x = 0; int curr_y = 0;
-int max_x = curr_x; int max_y = curr_y;
-int min_x = curr_x; int min_y = curr_y;
-
-// finished
-int backward_counter = 0;
-
-// lower
-const int lower_speed = 50;
-int lower_counter = 0;
-
-// ultrasonic sensor setup
-const int numSensors = 4;
-const float blockThreshold = 50;
-
-int trigPins[] = {2, 4, 0, 6}; //*
-int echoPins[] = {3, 5, 1, 7}; //*
-
-float sensorDists[] = {0.0, 0.0, 0.0, 0.0};
-
-NewPing sonar0(trigPins[0], echoPins[0], MAX_DISTANCE);
-NewPing sonar1(trigPins[1], echoPins[1], MAX_DISTANCE);
-NewPing sonar2(trigPins[2], echoPins[2], MAX_DISTANCE);
-NewPing sonar3(trigPins[3], echoPins[3], MAX_DISTANCE);
-
-NewPing sonars[] = {sonar0, sonar1, sonar2, sonar3};
-
-
-// state variables
+// STATE ENUM
 enum state_enum {
   START,
   LIFTOFF,
@@ -74,6 +39,7 @@ enum state_enum {
   TERMINATE
 } state = START;
 
+// MOTOR ENUM
 enum motor_enum {
   OFF,
   LIFT,
@@ -82,39 +48,72 @@ enum motor_enum {
   DROP
 } motor = OFF;
 
-int front() { return orientation % 4; }
-int right() { return (orientation + 1) % 4; }
-int back()  { return (orientation + 3) % 4; }
-int left()  { return (orientation + 2) % 4; }
+// PIN CONFIGURATION
+int trigPins[] = {2, 4, 0, 6};
+int echoPins[] = {3, 5, 1, 7};
+
+// ULTRASONIC SENSOR SETUP
+float sensorDists[] = {0.0, 0.0, 0.0, 0.0};
+
+NewPing sonar0(trigPins[0], echoPins[0], MAX_DISTANCE);
+NewPing sonar1(trigPins[1], echoPins[1], MAX_DISTANCE);
+NewPing sonar2(trigPins[2], echoPins[2], MAX_DISTANCE);
+NewPing sonar3(trigPins[3], echoPins[3], MAX_DISTANCE);
+
+NewPing sonars[] = {sonar0, sonar1, sonar2, sonar3};
+
+// POINT
+bool point_delay = false;
+
+//STATES:
+
+// START
+bool armed = false;
+bool lblock = false;
+bool fblock = false;
+
+// LIFTOFF
+int liftoff_counter = 0;
+
+// MOVE_FORWARD
+int forward_counter = 0;
+
+// WALL_LEFT
+int orientation = 0;
+int num_turns = 0;
+int curr_x = 0; int curr_y = 0;
+int max_x = curr_x; int max_y = curr_y;
+int min_x = curr_x; int min_y = curr_y;
+int wall_left_iter = 0;
+
+//OPEN
+int open_iter = 0;
+
+// FINISHED
+int backward_counter = 0;
+
+// LOWER
+int lower_counter = 0;
 
 void setup() {
   Serial.begin(9600);
   while(!Serial);
   armed = true;
 
-  for (int i = 0; i < numSensors; i++) {
+  for (int i = 0; i < NUM_SENSORS; i++) {
     resetSensor(echoPins[i]);
   }
 }
 
 void loop() {
-  Serial.println("testing");
-
-  switch (state) {
-    case START: Serial.println("*START"); break;
-    case LIFTOFF: Serial.println("*LIFTOFF"); break;
-    case MOVE_FORWARD: Serial.println("*MOVE_FORWARD"); break;
-    case WALL_LEFT: Serial.println("*WALL_LEFT"); break;
-    case OPEN: Serial.println("*OPEN"); break;
-    case BLOCKED: Serial.println("*BLOCKED"); break;
-    case FINISHED: Serial.println("*FINISHED"); break;
-    case LOWER: Serial.println("*LOWER"); break;
-    case TERMINATE: Serial.println("*TERMINATE"); break; 
-  }
+  Serial.print("orientation: "); Serial.println(orientation);
   
+  fblock = frontBlocked();
+  Serial.print("front blocked: "); Serial.println(fblock);
 
   lblock = leftBlocked(); 
-  fblock = frontBlocked();
+  Serial.print("left blocked: "); Serial.println(lblock);
+
 
 
   switch (state) {
@@ -163,20 +162,24 @@ void loop() {
         state = FINISHED;
         rotateRight();
         motor = FORWARD; motorUpdate(motor);
-      } else if (lblock && fblock) {
+
         // rotateRight() and transition to BLOCKED
+      } else if (lblock && fblock && wall_left_iter >= WALL_LEFT_THRESH) {
         state = BLOCKED;
         rotateRight(); num_turns += 1;
         motor = FORWARD; motorUpdate(motor);
-        // pointUpdate(curr_x, curr_y);
-      } else if ((!lblock) && (!fblock)) {
+        wall_left_iter = 0;
+
         // rotateLeft() and transition to OPEN
+      } else if ((!lblock) && (!fblock) && wall_left_iter >= WALL_LEFT_THRESH) { 
         state = OPEN;
         rotateLeft(); num_turns -= 1;
         motor = FORWARD; motorUpdate(motor);
-        // pointUpdate(curr_x, curr_y);
-      } else {
+        wall_left_iter = 0;
+
         // maintain distance and stay in WALL_LEFT
+      } else { 
+        wall_left_iter += 1;
         state = WALL_LEFT; // lblock && (!fblock)
         if ((!lblock) && fblock) { // BLOCKED_FRONT case
           rotateRight(); num_turns += 1; 
@@ -192,20 +195,27 @@ void loop() {
     }
 
     case OPEN: {
-      if (lblock && fblock) {
+      if (lblock && fblock && open_iter >= OPEN_THRESH) {
         state = BLOCKED;
         motor = FORWARD; motorUpdate(motor);
+        open_iter = 0;
+        
         // pointUpdate(curr_x, curr_y);
-      } else if (lblock) {
+      } else if (lblock && open_iter >= OPEN_THRESH) {
         state = WALL_LEFT; // case: entering wall_left off left turn
         motor = FORWARD; motorUpdate(motor);
+        open_iter = 0;
+        
         // pointUpdate(curr_x, curr_y);
-      } else if (fblock) {
+      } else if (fblock && open_iter >= OPEN_THRESH) {
         state = WALL_LEFT; // case: readjustment from wall_left to wall_left
         rotateRight(); num_turns += 1;
         motor = FORWARD; motorUpdate(motor);
+        open_iter = 0;
+        
         // pointUpdate(curr_x, curr_y);
       } else {
+        open_iter += 1;
         state = WALL_LEFT;
         motor = FORWARD; motorUpdate(motor);
         curr_x = curr_x + (orientation % 2) * (2 - orientation);
@@ -237,8 +247,8 @@ void loop() {
     case BLOCKED: {
       // bool lblock = leftBlocked();
       // bool fblock = frontBlocked();
-      state = WALL_LEFT;
       motor = FORWARD; motorUpdate(motor);
+      state = WALL_LEFT;
       break;
     }
 
@@ -275,9 +285,27 @@ void loop() {
     }
   }
 
+  //debug check state after measuremnt taken
+  switch (state) {
+    case START: Serial.println("*START"); break;
+    case LIFTOFF: Serial.println("*LIFTOFF"); break;
+    case MOVE_FORWARD: Serial.println("*MOVE_FORWARD"); break;
+    case WALL_LEFT: Serial.println("*WALL_LEFT"); break;
+    case OPEN: Serial.println("*OPEN"); break;
+    case BLOCKED: Serial.println("*BLOCKED"); break;
+    case FINISHED: Serial.println("*FINISHED"); break;
+    case LOWER: Serial.println("*LOWER"); break;
+    case TERMINATE: Serial.println("*TERMINATE"); break; 
+  }
   // pointUpdate(curr_x, curr_y);
+  Serial.println("\n");
   delay(200);
 }
+
+int front() { return orientation % 4; }
+int right() { return (orientation + 1) % 4; }
+int back()  { return (orientation + 2) % 4; }
+int left()  { return (orientation + 3) % 4; }
 
 void resetSensor(int index) {
   Serial.print("MAX: resetting sensor - echo: ");
@@ -289,12 +317,7 @@ void resetSensor(int index) {
 }
 void measureSensor(int index) {
   float dist = sonars[index].ping_cm();
-  if (dist == 0) {
-    resetSensor(echoPins[index]); delay(15);
-    dist = sonars[index].ping_cm();
-  }
   sensorDists[index] = dist;
-  Serial.print("DIST: "); Serial.print(index); Serial.print(" "); Serial.println(dist);
 }
 
 void motorUpdate(motor_enum m) {} //*
@@ -319,15 +342,39 @@ void rotateLeft() {
 }
 
 bool frontBlocked() {
-  int index = front();
-  measureSensor(index);
-  float dist = sensorDists[index];
-  return dist < blockThreshold && dist > 0;
+  blocked(front());
 }
 
 bool leftBlocked() {
-  int index = left();
+  blocked(left());
+}
+
+bool blocked(int index) {
+  RunningMedian medianFilter = RunningMedian(NUM_READS);
   measureSensor(index);
   float dist = sensorDists[index];
-  return dist < blockThreshold;
+  medianFilter.add(dist);
+  for (int i = 1; i < NUM_READS; i++) {
+    delay(150);
+    measureSensor(index);
+    dist = sensorDists[index];
+    medianFilter.add(dist);
+  }
+  float median = medianFilter.getMedian();
+  Serial.print("Median: "); Serial.println(median);
+  return median < BLOCK_THRESHOLD && median > 0;
 }
+
+//bool frontBlocked() {
+//  int index = front();
+//  measureSensor(index);
+//  float dist = sensorDists[index];
+//  return dist < BLOCK_THRESHOLD && dist > 0;
+//}
+//
+//bool leftBlocked() {
+//  int index = left();
+//  measureSensor(index);
+//  float dist = sensorDists[index];
+//  return dist < BLOCK_THRESHOLD;
+//}
--- ultrasonic_test/ultrasonic_test.ino
+++ ultrasonic_test/ultrasonic_test.ino
@@ -13,9 +13,9 @@
 
 #define MAX_DISTANCE 300
 
-int trigPin0 = 0;  int echoPin0 = 1; // back
-int trigPin1 = 2;  int echoPin1 = 3; // front
-int trigPin2 = 4;  int echoPin2 = 5;  // right
+int trigPin0 = 2;  int echoPin0 = 3; // front
+int trigPin1 = 4;  int echoPin1 = 5; // right
+int trigPin2 = 0;  int echoPin2 = 1;  // back
 int trigPin3 = 6; int echoPin3 = 7; // left
 
 
@@ -37,35 +37,35 @@ void setup() {
 void loop() {
   delay(1000);
   dist0 = sonar0.ping_cm();
-  if (dist0 == 0) {
-    resetSensor(echoPin0);
-    delay(15);
-    dist0 = sonar0.ping_cm();
-  }
+//  if (dist0 == 0) {
+//    resetSensor(echoPin0);
+//    delay(15);
+//    dist0 = sonar0.ping_cm();
+//  }
   Serial.print("dist0: "); Serial.println(dist0);
 
   dist1 = sonar1.ping_cm();
-  if (dist1 == 0) {
-    resetSensor(echoPin1);
-    delay(15);
-    dist1 = sonar1.ping_cm();
-  }
+//  if (dist1 == 0) {
+//    resetSensor(echoPin1);
+//    delay(15);
+//    dist1 = sonar1.ping_cm();
+//  }
   Serial.print("dist1: "); Serial.println(dist1);
 
   dist2 = sonar2.ping_cm();
-  if (dist2 == 0) {
-    resetSensor(echoPin2);
-    delay(15);
-    dist2 = sonar2.ping_cm();
-  }
+//  if (dist2 == 0) {
+//    resetSensor(echoPin2);
+//    delay(15);
+//    dist2 = sonar2.ping_cm();
+//  }
   Serial.print("dist2: "); Serial.println(dist2);
 
   dist3 = sonar3.ping_cm();
-  if (dist3 == 0) {
-    resetSensor(echoPin3);
-    delay(15);
-    dist3 = sonar3.ping_cm();
-  }
+//  if (dist3 == 0) {
+//    resetSensor(echoPin3);
+//    delay(15);
+//    dist3 = sonar3.ping_cm();
+//  }
   Serial.print("dist3: "); Serial.println(dist3);
 
   Serial.println();
